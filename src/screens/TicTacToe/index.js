71e5b679c5631fd61d34d6227d2d8452406b0cfc
elionselimaj <eselimaj@ritech.co>
2021-04-22 20:56:06 +0200

    basic tic tac toe

--- src/screens/TicTacToe/index.js
+++ src/screens/TicTacToe/index.js
@@ -1,10 +1,171 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableOpacity, Button} from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default () => {
+    const [gameState, setGameState] = useState([[0,0,0],[0,0,0],[0,0,0]]);
+    const [currentPlayer, setCurrentPlayer] = useState(1);
+
+    const initalizeGame = () => {
+        setGameState([[0,0,0],[0,0,0],[0,0,0]])
+    };
+    const getWinner = () => {
+        const NUMTILES = 3; // 5
+        let sum;
+        const arr = gameState;
+        for(let i = 0; i < NUMTILES; i++){
+            sum = arr[i][0] + arr[i][1] + arr[i][2];
+            if(sum === 3){
+                return 1;
+            } else if (sum === -3) {
+                return -1;
+            }
+        }
+
+        for(let i = 0; i < NUMTILES; i++){
+            sum = arr[0][i] + arr[1][i] + arr[2][i];
+            if(sum === 3){
+                return 1;
+            } else if (sum === -3) {
+                return -1;
+            }
+        }
+
+        sum = arr[0][0] + arr[1][1] + arr[2][2];
+        if(sum === 3){
+            return 1;
+        } else if (sum === -3) {
+            return -1;
+        }
+
+        sum = arr[2][0] + arr[1][1] + arr[0][2];
+        if(sum === 3){
+            return 1;
+        } else if (sum === -3) {
+            return -1;
+        }
+
+        return 0;
+
+    };
+
+    const onNewGame = () => {
+        initalizeGame();
+    };
+
+    const onTilePress = (row, col) => {
+       let value = gameState[row][col];
+        if (value !== 0) {
+            console.log('value !== 0');
+            return;
+        }
+        const player = currentPlayer;
+        const arr = gameState.slice();
+        arr[row][col] = player;
+        //setGameState({arr});
+        setGameState(arr);
+
+        const nextPlayer = (player === 1 ) ? -1: 1;
+        setCurrentPlayer(nextPlayer);
+
+        let winner = getWinner();
+        if (winner === 1) {
+            console.log('Player 1 is the winner');
+            initalizeGame();
+        }else if (winner === -1){
+            console.log('Player 2 is the winner');
+            initalizeGame();
+        }
+    };
+
+    const renderIcon = (row, col) => {
+        const value = gameState[row][col];
+        switch(value){
+            case 1: return <FontAwesome name="rocket" style={ styles.tileX } />;
+            case -1: return <FontAwesome name="star" style={ styles.tileO} />;
+            default: return <View />;
+        }
+    };
+
+
     return(
         <View style={{ flex: 1, justifyContent: 'center' , alignItems: 'center'}}>
-            <Text>TicTacToe</Text>
-        </View>
+                <View style={{flexDirection: "row"}}>
+                    <TouchableOpacity onPress={() => onTilePress(0,0)} style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0}]}>
+                        {renderIcon(0, 0)}
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => onTilePress(0,1)} style={[styles.tile, { borderTopWidth: 0}]} >
+                        {renderIcon(0, 1)}
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => onTilePress(0,2)} style={[styles.tile, { borderRightWidth: 0, borderTopWidth: 0}]}>
+                        {renderIcon(0, 2)}
+                    </TouchableOpacity>
+
+                </View>
+
+                <View style={{flexDirection: "row"}}>
+                    <TouchableOpacity onPress={() => onTilePress(1,0)} style={[styles.tile, { borderLeftWidth: 0}]} >
+                        {renderIcon(1, 0)}
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => onTilePress(1,1)} style={styles.tile}>
+                        {renderIcon(1, 1)}
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => onTilePress(1,2)} style={[styles.tile, { borderRightWidth: 0}]}>
+                        {renderIcon(1, 2)}
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{flexDirection: "row"}}>
+                    <TouchableOpacity onPress={() => onTilePress(2,0)} style={[styles.tile, { borderLeftWidth: 0, borderBottomWidth: 0}]}>
+                        {renderIcon(2, 0)}
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => onTilePress(2,1)} style={[styles.tile, { borderBottomWidth: 0}]} >
+                        {renderIcon(2, 1)}
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => onTilePress(2,2)} style={[styles.tile, { borderRightWidth: 0, borderBottomWidth: 0}]}>
+                        {renderIcon(2, 2)}
+                    </TouchableOpacity>
+                </View>
+
+                <Button title="New Game" onPress={onNewGame} style={styles.btn}/>
+            </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    tile: {
+        borderWidth: 5,
+        width: 100,
+        height: 100,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    tileX: {
+        color: "red",
+        fontSize: 50
+    },
+
+    tileO: {
+        color: "green",
+        fontSize: 50
+    },
+
+    btn: {
+        marginTop: 20,
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 20
+
+
+    }
+
+});
+
