d927e567711a0c2d5ff2f21630b768fcaf90cefe
elionselimaj <eselimaj@ritech.co>
2021-04-23 13:22:58 +0200

    tic tac toe 5x5

--- src/screens/TicTacToe/index.js
+++ src/screens/TicTacToe/index.js
@@ -1,51 +1,50 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, TouchableOpacity, Button} from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+import { StyleSheet, View, TouchableOpacity, Button, Alert} from 'react-native';
 import Colors from '../../constants/Colors';
 import whitePawn from '../../../assets/white_pawn.svg';
 import blackPawn from '../../../assets/black_pawn.svg';
 import SvgUri from 'react-native-svg-uri';
 
 export default () => {
-    const [gameState, setGameState] = useState([[0,0,0],[0,0,0],[0,0,0]]);
+    const [gameState, setGameState] = useState([[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]);
     const [currentPlayer, setCurrentPlayer] = useState(1);
 
     const initializeGame = () => {
-        setGameState([[0,0,0],[0,0,0],[0,0,0]])
+        setGameState([[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]])
     };
     const getWinner = () => {
-        const NUMTILES = 3; // 5
+        const NUMTILES = 5; // 5
         let sum;
         const arr = gameState;
         for(let i = 0; i < NUMTILES; i++){
-            sum = arr[i][0] + arr[i][1] + arr[i][2];
-            if(sum === 3){
+            sum = arr[i][0] + arr[i][1] + arr[i][2] + arr[i][3] + arr[i][4];
+            if(sum === 5){
                 return 1;
-            } else if (sum === -3) {
+            } else if (sum === -5) {
                 return -1;
             }
         }
 
         for(let i = 0; i < NUMTILES; i++){
-            sum = arr[0][i] + arr[1][i] + arr[2][i];
-            if(sum === 3){
+            sum = arr[0][i] + arr[1][i] + arr[2][i] + arr[3][i] + arr[4][i];
+            if(sum === 5){
                 return 1;
-            } else if (sum === -3) {
+            } else if (sum === -5) {
                 return -1;
             }
         }
 
-        sum = arr[0][0] + arr[1][1] + arr[2][2];
-        if(sum === 3){
+        sum = arr[0][0] + arr[1][1] + arr[2][2] + arr[3][3] + arr[4][4];
+        if(sum === 5){
             return 1;
-        } else if (sum === -3) {
+        } else if (sum === -5) {
             return -1;
         }
 
-        sum = arr[2][0] + arr[1][1] + arr[0][2];
-        if(sum === 3){
+        sum = arr[4][0] + arr[3][1] + arr[2][2] + arr[1][3] + arr[0][4];
+        if(sum === 5){
             return 1;
-        } else if (sum === -3) {
+        } else if (sum === -5) {
             return -1;
         }
 
@@ -74,10 +73,10 @@ export default () => {
 
         let winner = getWinner();
         if (winner === 1) {
-            console.log('Player 1 is the winner');
+            Alert.alert('Player 1 is the winner');
             initializeGame();
         }else if (winner === -1){
-            console.log('Player 2 is the winner');
+            Alert.alert('Player 2 is the winner');
             initializeGame();
         }
     };
@@ -109,10 +108,15 @@ export default () => {
                     <TouchableOpacity onPress={() => onTilePress(0,1)} style={[styles.tile, { borderTopWidth: 0, borderColor: Colors.primaryBlack}]} >
                         {renderIcon(0, 1)}
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => onTilePress(0,2)} style={[styles.tile, { borderRightWidth: 0, borderTopWidth: 0, borderColor: Colors.primaryBlack}]}>
+                    <TouchableOpacity onPress={() => onTilePress(0,2)} style={[styles.tile, { borderTopWidth: 0, borderColor: Colors.primaryBlack}]} >
                         {renderIcon(0, 2)}
                     </TouchableOpacity>
-
+                    <TouchableOpacity onPress={() => onTilePress(0,3)} style={[styles.tile, { borderTopWidth: 0, borderColor: Colors.primaryBlack}]} >
+                        {renderIcon(0, 3)}
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => onTilePress(0,4)} style={[styles.tile, { borderRightWidth: 0, borderTopWidth: 0, borderColor: Colors.primaryBlack}]}>
+                        {renderIcon(0, 4)}
+                    </TouchableOpacity>
                 </View>
 
                 <View style={{flexDirection: "row"}}>
@@ -122,20 +126,67 @@ export default () => {
                     <TouchableOpacity onPress={() => onTilePress(1,1)} style={[styles.tile, { borderColor: Colors.primaryBlack}]}>
                         {renderIcon(1, 1)}
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => onTilePress(1,2)} style={[styles.tile, { borderRightWidth: 0, borderColor: Colors.primaryBlack}]}>
+                    <TouchableOpacity onPress={() => onTilePress(1,2)} style={[styles.tile, { borderColor: Colors.primaryBlack}]}>
                         {renderIcon(1, 2)}
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => onTilePress(1,3)} style={[styles.tile, { borderColor: Colors.primaryBlack}]}>
+                        {renderIcon(1, 3)}
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => onTilePress(1,4)} style={[styles.tile, { borderRightWidth: 0, borderColor: Colors.primaryBlack}]}>
+                        {renderIcon(1, 4)}
                     </TouchableOpacity>
                 </View>
 
+            <View style={{flexDirection: "row"}}>
+                <TouchableOpacity onPress={() => onTilePress(2,0)} style={[styles.tile, { borderLeftWidth: 0, borderColor: Colors.primaryBlack}]} >
+                    {renderIcon(2, 0)}
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => onTilePress(2,1)} style={[styles.tile, { borderColor: Colors.primaryBlack}]}>
+                    {renderIcon(2, 1)}
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => onTilePress(2,2)} style={[styles.tile, { borderColor: Colors.primaryBlack}]}>
+                    {renderIcon(2, 2)}
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => onTilePress(2,3)} style={[styles.tile, { borderColor: Colors.primaryBlack}]}>
+                    {renderIcon(2, 3)}
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => onTilePress(2,4)} style={[styles.tile, { borderRightWidth: 0, borderColor: Colors.primaryBlack}]}>
+                    {renderIcon(2, 4)}
+                </TouchableOpacity>
+            </View>
+            <View style={{flexDirection: "row"}}>
+                <TouchableOpacity onPress={() => onTilePress(3,0)} style={[styles.tile, { borderLeftWidth: 0, borderColor: Colors.primaryBlack}]} >
+                    {renderIcon(3, 0)}
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => onTilePress(3,1)} style={[styles.tile, { borderColor: Colors.primaryBlack}]}>
+                    {renderIcon(3, 1)}
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => onTilePress(3,2)} style={[styles.tile, { borderColor: Colors.primaryBlack}]}>
+                    {renderIcon(3, 2)}
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => onTilePress(3,3)} style={[styles.tile, { borderColor: Colors.primaryBlack}]}>
+                    {renderIcon(3, 3)}
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => onTilePress(3,4)} style={[styles.tile, { borderRightWidth: 0, borderColor: Colors.primaryBlack}]}>
+                    {renderIcon(3, 4)}
+                </TouchableOpacity>
+            </View>
+
                 <View style={{flexDirection: "row"}}>
-                    <TouchableOpacity onPress={() => onTilePress(2,0)} style={[styles.tile, { borderLeftWidth: 0, borderBottomWidth: 0, borderColor: Colors.primaryBlack}]}>
-                        {renderIcon(2, 0)}
+                    <TouchableOpacity onPress={() => onTilePress(4,0)} style={[styles.tile, { borderLeftWidth: 0, borderBottomWidth: 0, borderColor: Colors.primaryBlack}]}>
+                        {renderIcon(4, 0)}
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => onTilePress(2,1)} style={[styles.tile, { borderBottomWidth: 0, borderColor: Colors.primaryBlack}]} >
-                        {renderIcon(2, 1)}
+                    <TouchableOpacity onPress={() => onTilePress(4,1)} style={[styles.tile, { borderBottomWidth: 0, borderColor: Colors.primaryBlack}]} >
+                        {renderIcon(4, 1)}
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => onTilePress(2,2)} style={[styles.tile, { borderRightWidth: 0, borderBottomWidth: 0, borderColor: Colors.primaryBlack}]}>
-                        {renderIcon(2, 2)}
+                    <TouchableOpacity onPress={() => onTilePress(4,2)} style={[styles.tile, { borderBottomWidth: 0, borderColor: Colors.primaryBlack}]} >
+                        {renderIcon(4, 2)}
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => onTilePress(4,3)} style={[styles.tile, { borderBottomWidth: 0, borderColor: Colors.primaryBlack}]} >
+                        {renderIcon(4, 3)}
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => onTilePress(4,4)} style={[styles.tile, { borderRightWidth: 0, borderBottomWidth: 0, borderColor: Colors.primaryBlack}]}>
+                        {renderIcon(4, 4)}
                     </TouchableOpacity>
                 </View>
 
@@ -153,9 +204,9 @@ const styles = StyleSheet.create({
     },
 
     tile: {
-        borderWidth: 5,
-        width: 100,
-        height: 100,
+        borderWidth: 2,
+        width: 80,
+        height: 80,
         alignItems: 'center',
         justifyContent: 'center'
     },
