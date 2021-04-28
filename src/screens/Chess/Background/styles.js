c1c827ffd75c79d57132f90046bfa8b8508d5ea4
elionselimaj <eselimaj@ritech.co>
2021-04-28 15:51:24 +0200

    minor bug fixed

--- src/screens/Chess/Background/styles.js
+++ src/screens/Chess/Background/styles.js
@@ -3,7 +3,6 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   squareContainer: {
     flex: 1,
-    backgroundColor,
     padding: 4,
     justifyContent: "space-between",
   },
